8e0056bcdb43f26b5f79047b8df0001534d08b94
Lucjan Krzywdziński <lucjan.krzywdzinski@icloud.com>
2022-09-16 17:02:48 +0200

    Initial Commit

---
+++ PencilKitForFeedback/PencilKitForFeedbackApp.swift
@@ -0,0 +1,17 @@
+//
+//  PencilKitForFeedbackApp.swift
+//  PencilKitForFeedback
+//
+//  Created by Lucek Krzywdzinski on 16/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct PencilKitForFeedbackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
